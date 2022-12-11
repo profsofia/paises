096c33c5ec2c1a2cb61bb644e6c3bdc4125b5fe4
SofiaSchenone <sofiainesschenone@gmail.com>
2022-12-11 01:29:13 +0000

    Servicios e inferfaces

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
-  templateUrl: './por-pais.component.html',
-  styleUrls: ['./por-pais.component.css']
+  templateUrl: './por-pais.component.html'
 })
 export class PorPaisComponent {
+  termino : string ="hola";
+  hayError: boolean =false;
+  constructor(private paisService: PaisService){}
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp=>{
+        console.log(resp);
+      }, (err)=>{
+        console.log(err);
+        this.hayError=true;
+      })
+
+  }
 
 }
